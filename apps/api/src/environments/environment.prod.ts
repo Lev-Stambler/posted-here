4b5da991a4c35dbd6aca83b590dbdcce0f532793
Lev Stambler <levstamb@gmail.com>
2020-10-06 13:39:52 +0000

    untrack

--- apps/api/src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  DB: 'production',
-  DB_USER: 'backend-prod',
-  DB_PASSWORD: 'Q46XSpzmzufHocWG',
-};
--- apps/api/src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  DB_USER: 'backend-dev',
-  DB: 'development',
-  DB_PASSWORD: 'EMRBrsF7Hp8iWo3N',
-  production: false,
-};
